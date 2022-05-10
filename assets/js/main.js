268f4355e94171155f4379a16b8aea18b42f3c92
Kadi <gueneykadircan@gmail.com>
2022-05-10 14:52:42 +0200

    ready

---
+++ assets/js/main.js
@@ -0,0 +1,116 @@
+// console.log("text");
+
+// ==============================
+//Aufgabe Lev 1_4: Data Types 
+// ==============================
+
+// console.log(typeof ("John")); string
+// console.log(typeof (3.14)); number 
+// console.log(typeof (NaN)); number 
+// console.log(typeof (false)); boolean 
+// console.log(typeof ([1, 2, 3, 4])); object 
+// console.log(typeof { name: 'John', age: 34 }); object 
+// console.log(typeof (new Date())); object 
+// console.log(typeof function () { }); function 
+// console.log(typeof (null)); object 
+// console.log(typeof ("")); string 
+// console.log(typeof (3 + 2 == 5)); boolean 
+// console.log(typeof (3 + "3")); string 
+
+
+// ==============================
+// Aufgabe Lev 1_3: Data Types
+// ==============================
+
+// let i = 1;
+// let lastName = "Johnson";
+// let j = "2";
+// let status = true;
+// let hello = "hello";
+// let helloWorld = "hello";
+
+// console.log(typeof (i)); // number 
+// console.log(typeof (lastName)); //string 
+// console.log(typeof (j)); // string 
+// console.log(typeof (status)); // boolean 
+// console.log(typeof (hello)); // string 
+// console.log(typeof (helloWorld)); // string
+
+
+// ====================================================
+// Aufgabe Lev 1_8: Data Types / Arithmetic operators 
+// ====================================================
+//Teil A) 
+// let x = 20;
+// let y = 30;
+
+// console.log(x * 1 + y * 1);
+// console.log(y - x);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+
+// let z = 10;
+
+// console.log((x * y) / z);
+// let resultOne = ((x * y) / z);
+// console.log(resultOne);
+
+// ==============================
+// Teil B) 
+// ==============================
+
+// let a = 15;
+// let b = 9;
+
+// console.log(a % b);
+
+// let c = 20;
+// console.log((a * 1 + b * 1) * c);
+
+// let resultTwo = ((a * 1 + b * 1) * c);
+// console.log(resultTwo);
+
+// console.log(a++);
+// console.log(b--);
+
+// console.log(b - a);
+// let resultThree = (b - a);
+// console.log(resultThree);
+
+// console.log(resultOne % resultThree)
+
+// ==============================
+// Aufgabe Lev 1_14: Boolean 
+//==============================
+// console.log(Boolean(100)); true 
+// console.log(Boolean(3.14)); true 
+// console.log(Boolean(-15)); true 
+// console.log(Boolean("hello")); true 
+// console.log(Boolean("false")); true 
+// console.log(Boolean(1 + 7 + 3.14)); true  
+// console.log(Boolean(0)); false 
+// console.log(Boolean(-0)); false 
+// console.log(Boolean(NaN)); false 
+// console.log(Boolean(null)); false 
+// console.log(Boolean(false)); false 
+
+// ==============================
+// Aufgabe 1_15: Comparison 
+// ==============================
+
+// let num = 5;
+// let anotherNum = 8;
+
+// console.log(num == anotherNum); false 
+// console.log(num > anotherNum); false 
+// console.log(num < anotherNum); true 
+// console.log(num >= anotherNum); false 
+// console.log(num <= anotherNum); true 
+
+// console.log(num == 5); true 
+// console.log(num != 8); true 
+// console.log(num != "5"); false
+
+
+
